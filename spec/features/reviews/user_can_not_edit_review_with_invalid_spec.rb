9a83611d7bf4249f6fb7aa2006fcef94bc055061
Danny Ramos <danramos21@gmail.com>
2020-05-12 23:58:01 -0600

    User story 6 complete

--- spec/features/reviews/user_can_not_edit_review_with_invalid_spec.rb
+++ spec/features/reviews/user_can_not_edit_review_with_invalid_spec.rb
@@ -1,23 +1,22 @@
-# require 'rails_helper'
-#
-# RSpec.describe "When I fail to enter a title, a rating, and/or content in edit review" do
-#   it "I see a flash message indicating that I need to fill in form" do
-#     shelter = create(:shelter)
-#     review
-#     pet = create(:pet, shelter: shelter)
-#
-#     visit "/shelters/#{shelter.id}/reviews/#{review.id}/edit"
-#     expect(find_button("Edit Review")).to be_truthy
-#
-#     expect(current_path).to eq("/shelters/#{shelter.id}/new_review")
-#
-#     fill_in :review_title, with: "This place rocks!"
-#
-#     click_button("Submit")
-#     expect(current_path).to eq("/shelters/#{shelter.id}/new_review")
-#     expect(page).to have_content("Please fill out entire form")
-#   end
-# end
+require 'rails_helper'
+
+RSpec.describe "When I fail to enter a title, a rating, and/or content in edit review" do
+  it "I see a flash message indicating that I need to fill in form" do
+    shelter = create(:shelter)
+    review = create(:review, shelter: shelter)
+
+    visit "/shelters/#{shelter.id}/reviews/#{review.id}/edit"
+    expect(find_button("Edit Review")).to be_truthy
+
+    expect(current_path).to eq("/shelters/#{shelter.id}/reviews/#{review.id}/edit")
+
+    fill_in :review_title, with: ""
+
+    click_button("Edit Review")
+    expect(current_path).to eq("/shelters/#{shelter.id}/reviews/#{review.id}/edit")
+    expect(page).to have_content("Please fill out entire form")
+  end
+end
 
 # When I fail to enter a title, a rating, and/or content
 # in the edit shelter review form, but still try to submit the form
