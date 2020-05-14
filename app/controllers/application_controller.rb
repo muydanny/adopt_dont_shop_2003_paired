6ca6e7117e43c0f8a522180670a275febfa41c3d
Ross Perry <perryr16@gmail.com>
2020-05-14 10:29:28 -0600

    testing favorite layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :get_favorite#, only: [:new, :create]
-
+  # helper_method :favorite
   add_flash_types :error
 
   private
 
    def get_favorite
      @favorite = Favorite.new(session[:favorite])
+   end
+
+   def favorite
+     @favorite ||= Favorite.new(session[:favorite])
    end
 
 end
