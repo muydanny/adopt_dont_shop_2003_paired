eced029139bf198119ec5ca137be00afbc741d7f
Ross Perry <perryr16@gmail.com>
2020-05-14 10:33:22 -0600

    refactor for helper method favorite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :get_favorite#, only: [:new, :create]
-  # helper_method :favorite
+  # before_action :get_favorite#, only: [:new, :create]
+  helper_method :favorite
   add_flash_types :error
 
   private
 
-   def get_favorite
-     @favorite = Favorite.new(session[:favorite])
-   end
+   # def get_favorite
+   #   @favorite = Favorite.new(session[:favorite])
+   # end
 
    def favorite
      @favorite ||= Favorite.new(session[:favorite])
