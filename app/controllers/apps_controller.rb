5b35d815f0e8c6e24f677e71ec47c26e2058c8da
Danny Ramos <danramos21@gmail.com>
2020-05-18 22:11:28 -0600

    New shelter page

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -18,6 +18,9 @@ class AppsController < ApplicationController
 
   def create
     @app = App.create(app_params)
+    if @app.image == nil
+      @app.assign_random_image
+    end
 
     if @app.save
       params[:pet_ids].each do |id|
